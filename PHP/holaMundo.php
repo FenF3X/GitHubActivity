4853214ba65111122014a3e8816eda56fb990275
FenFex <FenFex@alu.com>
2024-11-04 14:09:59 +0100

    Documentation PHP exercise

---
+++ PHP/holaMundo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is a sample function
+ *
+ * @param string $name
+ * @return string
+ */
+function sayHello($name) {
+    return "Hello, " . $name;
+}
---
+++ PHP/suma.php
@@ -0,0 +1,11 @@
+/**
+ * Suma dos números.
+ *
+ * @param int $a Primer número.
+ * @param int $b Segundo número.
+ * @return int Resultado de la suma.
+ */
+function suma($a, $b) {
+    return $a + $b;
+}
+
